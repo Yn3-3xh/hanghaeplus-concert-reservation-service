25cfe3bd97d82d94d5f0b1e59fd47d6642b981a4
Yn3-3xh <tong13579@naver.com>
2024-10-07 16:10:15 +0900

    initial settings

---
+++ src/main/java/hanghaeplus/concert/HanghaeplusConcertReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package hanghaeplus.concert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeplusConcertReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeplusConcertReservationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hanghaeplus/concert/HanghaeplusConcertReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghaeplus.concert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeplusConcertReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
