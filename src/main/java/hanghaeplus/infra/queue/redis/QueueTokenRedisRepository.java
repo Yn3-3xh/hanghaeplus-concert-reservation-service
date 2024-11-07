ec6225d67ee6965b20248a43192f8548a82922ee
Yn3-3xh <tong13579@naver.com>
2024-11-08 07:50:35 +0900

    step14/create: redisRepository

--- src/main/java/hanghaeplus/infra/queue/redis/QueueTokenRedisRepository.java
+++ src/main/java/hanghaeplus/infra/queue/redis/QueueTokenRedisRepository.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class QueueTokenRedisRepository {
 
+    
     private final RedisTemplate<String, String> redisTemplate;
 
     private ZSetOperations<String, String> zSetOperations;
